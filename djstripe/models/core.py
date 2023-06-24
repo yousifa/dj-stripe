a4854940ad3c73ebe9815aecffc532fdb3c4188b
Yousif Astarabadi <yousifa@gmail.com>
2023-06-23 18:22:38 -0700

    removed uniqeness check

--- djstripe/models/core.py
+++ djstripe/models/core.py
@@ -751,8 +751,8 @@ class Customer(StripeModel):
     )
     date_purged = models.DateTimeField(null=True, editable=False)
 
-    class Meta(StripeModel.Meta):
-        unique_together = ("subscriber", "livemode", "djstripe_owner_account")
+    # class Meta(StripeModel.Meta):
+    #     unique_together = ("subscriber", "livemode", "djstripe_owner_account")
 
     def __str__(self):
         if self.subscriber:
